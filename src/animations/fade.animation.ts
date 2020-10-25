617b0ecfab998ccf4bde846cd230c063899cdb70
Joseph Eborall <josepheborall@gmail.com>
2020-10-24 20:21:42 -0600

    Polish

--- src/animations/fade.animation.ts
+++ src/animations/fade.animation.ts
@@ -9,18 +9,18 @@ import {
 
 export const fadeAnimation =
   trigger('fadeAnimation', [
-    transition('* <=> *', [
-      query(':enter, :leave', [
-        style({
-          opacity: 0,
-          position: 'absolute',
-          transform: 'translateY(15px)',
-        })
-      ]),
-      query(':enter', [
-        animate('300ms cubic-bezier(0.4, 0.0, 0.2, 1)',
-          style({transform: 'translateY(-15px)', opacity: 1})
-        )
-      ])
-    ])
+    // transition('* => *', [
+    //   query(':enter, :leave', [
+    //     style({
+    //       opacity: 0,
+    //       // position: 'absolute',
+    //       // transform: 'translateY(15px)',
+    //     })
+    //   ]),
+    //   query(':enter', [
+    //     animate('300ms cubic-bezier(0.4, 0.0, 0.2, 1)',
+    //       style({transform: 'translateY(-15px)', opacity: 1})
+    //     )
+    //   ])
+    // ])
   ]);
--- src/app/portfolio-list/portfolio-list.component.ts
+++ src/app/portfolio-list/portfolio-list.component.ts
@@ -13,7 +13,7 @@ import {trigger, transition, style, animate, query, stagger, keyframes} from '@a
 
     trigger('photosAnimation', [
       transition('* => *', [
-        query('.project', style({ opacity: 0, transform: 'translateY(15px)'})),
+        query(':enter, :exit', style({ opacity: 0, transform: 'translateY(15px)'})),
         query('.project',
           stagger('50ms', [
             animate('230ms cubic-bezier(0.4, 0.0, 0.2, 1)', style({opacity: 1, transform: 'translateY(-15px)'}))
